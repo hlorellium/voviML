9fdac2cb1eb1e4b2d6b21c52be3c404f84f9fb93
hlorellium <tdoregani@hotmail.com>
2020-09-06 22:08:05 +0300

    Fix some

--- src/components/ContactUsForm.js
+++ src/components/ContactUsForm.js
@@ -48,7 +48,6 @@ export const ContactUsForm = ({ contactUs }) => {
                     });
                 alert(values);
                 } catch (error) {
-                    alert(error);
                 }
             }}
         >
